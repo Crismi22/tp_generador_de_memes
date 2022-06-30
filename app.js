5e5da28e4147f476f723b911a03beaa2e46d6173
Cristina <97647142+Crismi22@users.noreply.github.com>
2022-06-29 23:39:37 -0300

    terminando estilados inputs - texto y crear btn darkMode js

---
+++ app.js
@@ -0,0 +1,8 @@
+// primer intento de modo oscuro
+
+const dark = document.getElementById('');
+dark.addEventListener('click', () =>{
+    event.preventDefault();
+    document.body.classList.toggle('modo-oscuro');
+})
+
