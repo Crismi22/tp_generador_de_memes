3028d2ecc75d26686b90ef9bcf8413d3656324c0
Cristina <97647142+Crismi22@users.noreply.github.com>
2022-07-01 19:26:25 -0300

    se realizaron pruebas para input modificable de caja meme

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-// primer intento de modo oscuro
+// PRIMER INTENTO DE MODO OSCURO
 
-const dark = document.getElementById('');
-dark.addEventListener('click', () =>{
-    event.preventDefault();
-    document.body.classList.toggle('modo-oscuro');
+// const btnLight = document.getElementById('button-modo-dark')
+// btnLight.addEventListener('click', () =>{
+//     document.body.classList.toggle('.dark-dark');
+//     btnLight.classList.toggle('.modo-light')
+// })
+
+// DALE KRIS!! VOS PODES CON ESTO! SOS INTELIGENTE, SOS CAPAZ Y TE LO MERECES PARA SEGUIR AVANZANDO Y CRECIENDO. FALTA CADA VEZ MENOS PARA EL FUTURO QUE ESTAS ESPERANDO FELIZ
+
+//INTENTO PARTE INPUT TEXTO CAJA MEME////
+
+// const actualizarTextos = () => {
+//     $('top-text').innerText = $('top-text').value;
+//   }
+// const inicializarTexto = () => {
+//     $('top-text').addEventListener('input', actualizarTextos)
+// }
+const input = document.getElementById('input')
+const supText = document.getElementById('textoTop'); 
+// const infTExt = document.getElementById('textoInf');
+
+input.addEventListener('input', (event) => {
+    const textoIngresado = event.target.value;
+    supText.innerHTML = textoIngresado;
 })
 
+// input.addEventListener('input', (event) => {
+//     const textoIngresado2 = event.target.value;
+//     textoInf.innerHTML = textoIngresado2;
+// })
