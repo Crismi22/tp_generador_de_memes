215ccc0ac15b0274e99c099ebf345f695392d35a
Cristina <97647142+Crismi22@users.noreply.github.com>
2022-07-02 16:20:24 -0300

    checkbox´s de la caja meme listos.

--- app.js
+++ app.js
@@ -6,30 +6,54 @@
 //     btnLight.classList.toggle('.modo-light')
 // })
 
+// *********************************
+// CAMBIO DE COLUMNA IMAGEN A COLUMNA TEXTO CON BOTONES NAV 
+// *********************
+
 const btnImg = document.getElementById('button-text-img');
 const btnText = document.getElementById('button-text-text');
 
 const panelImagen = document.getElementById('panel-imagen');
 const panelTexto = document.getElementById('panel-texto');
 
+btnImg.addEventListener('click', () => {
+    panelImagen.style.display = "block";
+    panelTexto.style.display = "none";
+})
+
+btnText.addEventListener('click',() => {
+    panelTexto.style.display = "block";
+    panelImagen.style.display = "none";
+})
 
 
 // DALE KRIS!! VOS PODES CON ESTO! SOS INTELIGENTE, SOS CAPAZ Y TE LO MERECES PARA SEGUIR AVANZANDO Y CRECIENDO. FALTA CADA VEZ MENOS PARA EL FUTURO QUE ESTAS ESPERANDO FELIZ
+// **************************************
+//+++++PARTE INPUT TEXTO CAJA MEME | CHECKBOX CAJA MEME////
+// **************************************
+const inputTop = document.getElementById('inputTop');
+const inputBut = document.getElementById('inputBut');
+const textoTop = document.getElementById('textoTop'); 
+const textoInf = document.getElementById('textoInf');
 
-//PARTE INPUT TEXTO CAJA MEME////
+const check1 = document.getElementById('not-top');
+const check2 = document.getElementById('not-button');
 
-const inputTop = document.getElementById('inputTop')
-const supText = document.getElementById('textoTop'); 
-const infText = document.getElementById('textoInf');
-const inputB = document.getElementById('inputB')
-
+// const check3 = document.getElementById('sin-fondo');
 
 inputTop.addEventListener('input', (event) => {
     const textoIngresado = event.target.value;
-    supText.innerHTML = textoIngresado;
+    textoTop.innerHTML = textoIngresado;
+})
+inputBut.addEventListener('input', (event) => {
+    const textoIngresado2 = event.target.value;
+    textoInf.innerHTML = textoIngresado2;
 })
 
-inputB.addEventListener('input', (event) => {
-    const textoIngresado2 = event.target.value;
-    infText.innerHTML = textoIngresado2;
+
+check1.addEventListener('click', () => {
+    textoTop.classList.toggle('ocultar');
+}) 
+check2.addEventListener('click', () => {
+    textoInf.classList.toggle('ocultar');
 })
