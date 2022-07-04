4deecee4f1be1a97e444f029ef86ebb27e0ea249
Cristina <97647142+Crismi22@users.noreply.github.com>
2022-07-03 22:35:00 -0300

    funcion: cambio de tipo de fuente ok

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-// *****************************************
-////             MODO OSCURO        ////
+// ********************************************
+//                 MODO OSCURO        
+// ********************************************
+
 const light = document.getElementById('button-modo-dark');
 const body = document.body;
+
+const valor = localStorage.getItem("light")
 
 light.addEventListener('click',()=> {
     const val = body.classList.toggle("modo-light")
     localStorage.setItem('light',val)
 })
 
-
 // *****funcion para que al actualizar pagina siga con el modo elegido por el usuario*******
-const valor = localStorage.getItem("light")
-
 if (valor == "true") {
     body.classList.add("modo-light")
 } else {
     body.classList.remove("modo-light")
 }
 
-// *********************************
+// ********************************************************
 // CAMBIO DE COLUMNA IMAGEN A COLUMNA TEXTO CON BOTONES NAV 
-// *********************
+// ********************************************************
 
 const btnImg = document.getElementById('button-text-img');
 const btnText = document.getElementById('button-text-text');
@@ -37,20 +38,19 @@ btnText.addEventListener('click',() => {
     panelImagen.style.display = "none";
 })
 
-
-// DALE KRIS!! VOS PODES CON ESTO! SOS INTELIGENTE, SOS CAPAZ Y TE LO MERECES PARA SEGUIR AVANZANDO Y CRECIENDO. FALTA CADA VEZ MENOS PARA EL FUTURO QUE ESTAS ESPERANDO FELIZ
-// **************************************
+// *******************************************************
 //+++++PARTE INPUT TEXTO CAJA MEME | CHECKBOX CAJA MEME////
-// **************************************
+// *******************************************************
+
 const inputTop = document.getElementById('inputTop');
 const inputBut = document.getElementById('inputBut');
 const textoTop = document.getElementById('textoTop'); 
 const textoInf = document.getElementById('textoInf');
+const cajaMeme = document.getElementById('caja-imagen');
 
 const check1 = document.getElementById('not-top');
 const check2 = document.getElementById('not-button');
-
-// const check3 = document.getElementById('sin-fondo');
+const check3 = document.getElementById('sin-fondo');
 
 inputTop.addEventListener('input', (event) => {
     const textoIngresado = event.target.value;
@@ -68,3 +68,31 @@ check1.addEventListener('click', () => {
 check2.addEventListener('click', () => {
     textoInf.classList.toggle('ocultar');
 })
+
+check3.addEventListener('click', () => {
+    cajaMeme.classList.toggle('transparente');
+})
+
+
+// DALE KRIS!! VOS PODES CON ESTO! SOS INTELIGENTE, SOS CAPAZ Y TE LO MERECES PARA SEGUIR AVANZANDO Y CRECIENDO. FALTA CADA VEZ MENOS PARA EL FUTURO QUE ESTAS ESPERANDO FELIZ
+
+// *******************************************************************
+// CAMBIO TIPO DE FUENTE
+// *******************************************************************
+
+const cambioFuente = function (font) {
+    document.getElementById('textoTop').style.fontFamily = font.value;
+    document.getElementById('textoInf').style.fontFamily = font.value;
+}
+
+
+
+// ********************
+// TAMAÑO DE FUENTE
+// ************************
+
+
+
+// *****************************
+// ALINEACION DE TEXTO 
+// *****************************
